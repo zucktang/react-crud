23f8a46c6e377153177caa7b7c4ead9c301e2dc0
zucktang <zucktang@gmail.com.com>
2022-09-12 15:53:27 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="App container">
+      <h1>Ticket</h1>
+      <div className="Ticket">
+        <form action = "">
+          <div className="mb-3">
+              
+          </div>
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,90 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let mysql = require('mysql');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+let db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'ticketmanagement',
+})
+db.connect(()=>{
+    console.log('Connected')});
+
+app.get('/ticket', (req, res)=>{
+    db.query('SELECT * FROM ticket_detail', (err,results,fields) =>{
+        let message = "";
+        if(results===undefined || results.length===0){
+            message = "Ticket is Empty"
+        }
+        else{
+            message = "here all tickets"
+        }
+        console.log(results)
+        return res.send({
+            data:results,
+            message
+        })
+    })
+})
+
+app.post('/ticket', (req, res)=>{
+    let title = req.body.title
+    let description = req.body.description
+    let contact_information= req.body.contact_information
+   
+
+    db.query('insert into ticket_detail (title, description,contact_information ) values (?,?,?)',[title, description, contact_information],(err,results,fields)=>
+    {
+    if(err)
+    {
+        console.log(err)
+    }
+    else{
+    return res.send({
+        data: results
+     })
+    }   
+    })
+})
+
+app.put('/ticket/:id',(req,res)=>{
+    let id = req.params.id
+    let title = req.body.title
+    let description = req.body.description
+    let contact_information= req.body.contact_information
+
+    if(!id){
+        return res.status(400).send({
+            error: "Please provide id"
+        }
+        )}
+    else{
+        db.query('update ticket_detail SET title = ? , description = ? , contact_information = ? where id = ? ', [title,description,contact_information,id],(err,results,fields)=>{
+            if(err){
+            console.log(err);
+            }
+            if(results.changedRows === 0){
+                message = "Ticket not found or data are same"
+            }
+            else{
+                message = "Ticket has updated"
+            }
+            return res.send({
+                data:results,
+                message
+            })
+        })
+    }
+})
+
+app.listen(3001,()=>{
+    console.log('App running on port 3001');
+})
+
+module.exports = app;
+
